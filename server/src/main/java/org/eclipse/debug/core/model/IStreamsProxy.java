5412253f303f87eb0e2f67d553c63e91eab5e2a7
Rob Stryker <rob@oxbeef.net>
2018-04-18 11:01:47 -0400

    JBIDE-25925 - initial support for launching a java program. Not yet complete.
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ server/src/main/java/org/eclipse/debug/core/model/IStreamsProxy.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.debug.core.model;
+
+
+import java.io.IOException;
+
+/**
+ * A streams proxy acts as proxy between the streams of a
+ * process and interested clients. This abstraction allows
+ * implementations of <code>IProcess</code> to handle I/O related
+ * to the standard input, output, and error streams associated
+ * with a process.
+ * <p>
+ * Clients implementing the <code>IProcess</code> interface must also
+ * provide an implementation of this interface.
+ * </p>
+ * @see IProcess
+ */
+public interface IStreamsProxy {
+	/**
+	 * Returns a monitor for the error stream of this proxy's process,
+	 * or <code>null</code> if not supported.
+	 * The monitor is connected to the error stream of the
+	 * associated process.
+	 *
+	 * @return an error stream monitor, or <code>null</code> if none
+	 */
+	public IStreamMonitor getErrorStreamMonitor();
+	/**
+	 * Returns a monitor for the output stream of this proxy's process,
+	 * or <code>null</code> if not supported.
+	 * The monitor is connected to the output stream of the
+	 * associated process.
+	 *
+	 * @return an output stream monitor, or <code>null</code> if none
+	 */
+	public IStreamMonitor getOutputStreamMonitor();
+	/**
+	 * Writes the given text to the output stream connected to the
+	 * standard input stream of this proxy's process.
+	 *
+	 * @param input the text to be written
+	 * @exception IOException when an error occurs writing to the
+	 *		underlying <code>OutputStream</code>.
+	 *
+	 */
+	public void write(String input) throws IOException;
+}
---
+++ server/src/main/java/org/jboss/tools/ssp/server/launch/internal/util/OSUtils.java
@@ -0,0 +1,32 @@
+package org.jboss.tools.ssp.server.launch.internal.util;
+
+public final class OSUtils {
+	private static String OS = System.getProperty("os.name").toLowerCase();
+
+	public static String getOsName() {
+		if (OS == null) {
+			OS = System.getProperty("os.name");
+		}
+		return OS;
+	}
+
+	public static boolean isWindows() {
+		return (OS.indexOf("win") >= 0);
+	}
+
+	public static boolean isMac() {
+		return (OS.indexOf("mac") >= 0);
+	}
+
+	public static boolean isUnix() {
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+	}
+
+	public static boolean isSolaris() {
+		return (OS.indexOf("sunos") >= 0);
+	}
+
+	public static boolean isUnknown() {
+		return !isWindows() && !isMac() && !isUnix() && !isSolaris();
+	}
+}
