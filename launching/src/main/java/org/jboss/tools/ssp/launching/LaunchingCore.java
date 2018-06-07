8d8932bb9eb419f7055c10a31e952fcc05b061a5
Andre Dietisheim <adietish@redhat.com>
2018-06-07 15:41:55 +0200

    LaunchingCore: added private constructor
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- launching/src/main/java/org/jboss/tools/ssp/launching/LaunchingCore.java
+++ launching/src/main/java/org/jboss/tools/ssp/launching/LaunchingCore.java
@@ -11,7 +11,16 @@ package org.jboss.tools.ssp.launching;
 import java.io.File;
 
 public class LaunchingCore {
-	private static LaunchingCore instance = new LaunchingCore();
+
+	public static final String SYSPROP_DATA_LOCATION = "org.jboss.tools.ssp.data";
+	public static final String SYSPROP_DATA_DEFAULT_LOCATION = ".org.jboss.tools.ssp.data";
+	private static final String SYSPROP_USER_HOME = "user.home";
+
+	private static final LaunchingCore instance = new LaunchingCore();
+	
+	private LaunchingCore() {
+	}
+
 	public static LaunchingCore getDefault() {
 		return instance;
 	}
@@ -19,26 +28,26 @@ public class LaunchingCore {
 	public static void log(Throwable t) {
 		t.printStackTrace();
 	}
+
 	public static void log(String bind) {
 		System.out.println(bind);
 	}
 	
-	public static final String SYSPROP_DATA_LOCATION = "org.jboss.tools.ssp.data";
-	public static final String SYSPROP_DATA_DEFAULT_LOCATION = ".org.jboss.tools.ssp.data";
 	public static File getDataLocation() {
+		File data = null;
 		String prop = System.getProperty(SYSPROP_DATA_LOCATION);
-		File ret = null;
-		if( prop != null ) {
-			ret = new File(prop);
+
+		if (prop != null) {
+			data = new File(prop);
+		} else {
+			File home = new File(System.getProperty(SYSPROP_USER_HOME));
+			data = new File(home, SYSPROP_DATA_DEFAULT_LOCATION);
 		}
-		if( prop == null ) {
-			String home = System.getProperty("user.home");
-			File home2 = new File(home);
-			ret = new File(home2, SYSPROP_DATA_DEFAULT_LOCATION);
+
+		if (data != null && !data.exists()) {
+			data.mkdirs();
 		}
-		if( ret != null && !ret.exists()) {
-			ret.mkdirs();
-		}
-		return ret;
+
+		return data;
 	}
 }
