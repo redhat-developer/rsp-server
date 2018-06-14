846bb9e1447ba8689af4db8a8a408e12a2ace5eb
Rob Stryker <rob@oxbeef.net>
2018-06-14 16:00:00 -0400

    Missing files

---
+++ schema/src/main/resources/schema/typescript/StartServerResponse.d.ts
@@ -0,0 +1,20 @@
+export interface StartServerResponse {
+    status: Status;
+    details: CommandLineDetails;
+}
+
+export interface Status {
+    severity: number;
+    code: number;
+    message: string;
+    trace: string;
+    ok: boolean;
+    plugin: string;
+}
+
+export interface CommandLineDetails {
+    cmdLine: string[];
+    workingDir: string;
+    envp: string[];
+    properties: { [index: string]: string };
+}
