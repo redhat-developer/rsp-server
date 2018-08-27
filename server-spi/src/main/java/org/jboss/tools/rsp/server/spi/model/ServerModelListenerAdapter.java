970af1cb059c3e77f7019389fd91165eaa86b178
Rob Stryker <rob@oxbeef.net>
2018-08-27 17:28:51 -0400

    More testing
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ server-spi/src/main/java/org/jboss/tools/rsp/server/spi/model/ServerModelListenerAdapter.java
@@ -0,0 +1,49 @@
+package org.jboss.tools.rsp.server.spi.model;
+
+import org.jboss.tools.rsp.api.dao.ServerHandle;
+
+public class ServerModelListenerAdapter implements IServerModelListener {
+
+	@Override
+	public void serverAdded(ServerHandle server) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverRemoved(ServerHandle server) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverAttributesChanged(ServerHandle server) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverStateChanged(ServerHandle server, int state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverProcessCreated(ServerHandle server, String processId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverProcessTerminated(ServerHandle server, String processId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void serverProcessOutputAppended(ServerHandle server, String processId, int streamType, String text) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- server/src/test/java/org/jboss/tools/rsp/server/model/ServerModelTest.java
+++ server/src/test/java/org/jboss/tools/rsp/server/model/ServerModelTest.java
@@ -1,18 +1,26 @@
 package org.jboss.tools.rsp.server.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.jboss.tools.rsp.api.dao.ServerHandle;
+import org.jboss.tools.rsp.api.dao.ServerType;
 import org.jboss.tools.rsp.eclipse.core.runtime.CoreException;
+import org.jboss.tools.rsp.server.spi.model.ServerModelListenerAdapter;
+import org.jboss.tools.rsp.server.spi.servertype.IServer;
+import org.jboss.tools.rsp.server.spi.servertype.IServerDelegate;
 import org.jboss.tools.rsp.server.spi.servertype.IServerType;
 import org.junit.Test;
 
@@ -104,6 +112,117 @@ public class ServerModelTest {
 			fail();
 		}
 	}
+	
+
+	@Test
+	public void testLoadServerHandles() {
+		ServerModel sm = new ServerModel();
+		sm.addServerType(mockServerType("wonka5"));
+		Path dir = null;
+		Path s1 = null;
+		try {
+			dir = Files.createTempDirectory("servermodeltest");
+			s1 = dir.resolve("s1");
+			String contents = getServerString("abc123", "wonka5"); 
+			Files.write(s1, contents.getBytes());
+			sm.loadServers(dir.toFile());
+			assertEquals(sm.getServers().size(), 1);
+			
+			assertNotNull(sm.getServerHandles());
+			assertEquals(sm.getServerHandles().length, 1);
+			ServerHandle sh = sm.getServerHandles()[0];
+			assertNotNull(sh);
+			assertEquals(sh.getId(), "abc123");
+			assertNotNull(sh.getType());
+			ServerType st = sh.getType();
+			assertEquals(st.getId(), "wonka5");
+		} catch(IOException | CoreException e) {
+			if( s1 != null && s1.toFile().exists()) {
+				s1.toFile().delete();
+				s1.toFile().getParentFile().delete();
+			}
+			fail();
+		}
+	}
+	
+	@Test
+	public void testAddAndRemoveServerType() {
+		ServerModel sm = new ServerModel();
+		assertNotNull(sm.getServerTypes());
+		assertEquals(0, sm.getServerTypes().length);
+		assertNull(sm.getIServerType("wonka5"));
+		
+		IServerType mock = mockServerType("wonka5");
+		sm.addServerType(mock);
+		assertNotNull(sm.getServerTypes());
+		assertEquals(1, sm.getServerTypes().length);
+		assertNotNull(sm.getIServerType("wonka5"));
+
+		sm.removeServerType(mock);
+		assertNotNull(sm.getServerTypes());
+		assertNull(sm.getIServerType("wonka5"));
+		assertEquals(0, sm.getServerTypes().length);
+	}
+	
+	@Test
+	public void testLoadAndRemoveServerWithListener() {
+		ServerModel sm = new ServerModel();
+		final Boolean[] added = new Boolean[] {new Boolean(false)};
+		final Boolean[] removed = new Boolean[] {new Boolean(false)};
+		ServerModelListenerAdapter smla = new ServerModelListenerAdapter() {
+			@Override
+			public void serverAdded(ServerHandle server) {
+				added[0] = Boolean.TRUE;
+			}
+			@Override
+			public void serverRemoved(ServerHandle server) {
+				removed[0] = Boolean.TRUE;
+			}
+		}; 
+		sm.addServerModelListener(smla);
+		sm.addServerType(mockServerType("wonka5"));
+		Path dir = null;
+		Path s1 = null;
+		try {
+			dir = Files.createTempDirectory("servermodeltest");
+			s1 = dir.resolve("s1");
+			String contents = getServerString("abc123", "wonka5"); 
+			Files.write(s1, contents.getBytes());
+			sm.loadServers(dir.toFile());
+			assertEquals(sm.getServers().size(), 1);
+			assertNotNull(sm.getServer("abc123"));
+			assertTrue(added[0].booleanValue());
+			sm.removeServer("abc123");
+			assertEquals(sm.getServers().size(), 0);
+			assertNull(sm.getServer("abc123"));
+			assertTrue(removed[0].booleanValue());
+			assertFalse(s1.toFile().exists());
+			
+			sm.removeServerModelListener(smla);
+			added[0] = false;
+			removed[0] = false;
+			
+			dir = Files.createTempDirectory("servermodeltest");
+			s1 = dir.resolve("s1");
+			contents = getServerString("abc123", "wonka5"); 
+			Files.write(s1, contents.getBytes());
+			sm.loadServers(dir.toFile());
+			assertEquals(sm.getServers().size(), 1);
+			assertNotNull(sm.getServer("abc123"));
+			assertFalse(added[0].booleanValue());
+			sm.removeServer("abc123");
+			assertEquals(sm.getServers().size(), 0);
+			assertNull(sm.getServer("abc123"));
+			assertFalse(removed[0].booleanValue());
+			assertFalse(s1.toFile().exists());
+		} catch(IOException | CoreException e) {
+			if( s1 != null && s1.toFile().exists()) {
+				s1.toFile().delete();
+				s1.toFile().getParentFile().delete();
+			}
+			fail();
+		}
+	}
 
 	
 	private String getServerString(String name, String type) {
@@ -122,7 +241,9 @@ public class ServerModelTest {
 	private IServerType mockServerType(String typeId) {
 		IServerType ist = mock(IServerType.class);
 		doReturn(typeId).when(ist).getId();
+		IServerDelegate isd = mock(IServerDelegate.class);
+		when(ist.createServerDelegate(any(IServer.class))).thenReturn(isd);
 		return ist;
 	}
-	
+
 }
