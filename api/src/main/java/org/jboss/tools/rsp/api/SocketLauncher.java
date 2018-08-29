0f3cf9cd584fa375751d2745f258d626775ca5f4
Rob Stryker <rob@oxbeef.net>
2018-08-28 22:59:08 -0400

    Enable tracing / logging to see json requests
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Use throws IOException instead try/catch
    
    Logging for stalled build / bad tests
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Big bug in logging code, caused failure to launch server
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- api/src/main/java/org/jboss/tools/rsp/api/SocketLauncher.java
+++ api/src/main/java/org/jboss/tools/rsp/api/SocketLauncher.java
@@ -9,6 +9,7 @@
 package org.jboss.tools.rsp.api;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
@@ -24,15 +25,25 @@ public class SocketLauncher<T> implements Launcher<T> {
 	private Socket socket;
 	
 
-	public SocketLauncher(Object localService, Class<T> remoteInterface, Socket socket) {
-		try {
-			this.launcher = Launcher.createLauncher(localService, remoteInterface, socket.getInputStream(), socket.getOutputStream());
-			this.socket = socket;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+	public SocketLauncher(Object localService, Class<T> remoteInterface, Socket socket) throws IOException {
+		this.launcher = Launcher.createLauncher(localService, remoteInterface, socket.getInputStream(), socket.getOutputStream());
+		this.socket = socket;
 	}
 
+	public SocketLauncher(Object localService, Class<T> remoteInterface, Socket socket, PrintWriter tracing) throws IOException {
+		Launcher<T> launcherTmp = new Builder<T>()
+				.setLocalService(localService)
+				.setRemoteInterface(remoteInterface)
+				.setInput(socket.getInputStream())
+				.setOutput(socket.getOutputStream())
+				.traceMessages(tracing)
+				.create();
+		this.launcher = launcherTmp;
+		this.socket = socket;
+	}
+
+	
+	
 	public CompletableFuture<Void> startListening() {
 		return CompletableFuture.runAsync(() -> {
 			try {
