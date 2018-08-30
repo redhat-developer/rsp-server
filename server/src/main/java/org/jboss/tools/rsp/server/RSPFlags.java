7eac0f5a21711c2973abc9323e750badc5c0d532
Rob Stryker <rob@oxbeef.net>
2018-08-30 12:43:54 -0400

    Added a test
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- server/src/main/java/org/jboss/tools/rsp/server/RSPFlags.java
+++ server/src/main/java/org/jboss/tools/rsp/server/RSPFlags.java
@@ -4,12 +4,14 @@ public class RSPFlags {
 	public static final String SYSPROP_LOG_LEVEL_FLAG = "rsp.log.level";
 	public static final String SYSPROP_SERVER_PORT = "rsp.server.port";
 	
+	public static final int DEFAULT_PORT = 27511;
+	
 	public static int getLogLevel() {
 		return getIntSysprop(SYSPROP_LOG_LEVEL_FLAG, RSPLogger.LOG_WARNING);
 	}
 
 	public static int getServerPort() {
-		return getIntSysprop(SYSPROP_SERVER_PORT, 27511);
+		return getIntSysprop(SYSPROP_SERVER_PORT, DEFAULT_PORT);
 	}
 
 	public static int getIntSysprop(String key, int def) {
