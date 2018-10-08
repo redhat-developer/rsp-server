22aa6223eac79712807c6322a288109d764855e4
Rob Stryker <rob@oxbeef.net>
2018-10-08 11:05:52 -0400

    Issue 88 - further refine capabilities API
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- api/bundles/org.jboss.tools.rsp.api/src/main/java/org/jboss/tools/rsp/api/dao/ServerCapabilitiesResponse.java
+++ api/bundles/org.jboss.tools.rsp.api/src/main/java/org/jboss/tools/rsp/api/dao/ServerCapabilitiesResponse.java
@@ -1,13 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
 package org.jboss.tools.rsp.api.dao;
 
 import java.util.Map;
 
 public class ServerCapabilitiesResponse {
 	private Map<String,String> serverCapabilities;
-	public ServerCapabilitiesResponse(Map<String,String> map) {
+	private Status clientRegistrationStatus;
+	public ServerCapabilitiesResponse(Status clientRegistrationStatus, Map<String,String> map) {
+		this.clientRegistrationStatus = clientRegistrationStatus;
 		this.serverCapabilities = map;
 	}
 	public Map<String,String> getMap() {
 		return serverCapabilities;
+	}
+	public Status getClientRegistrationStatus() {
+		return clientRegistrationStatus;
 	}
 }
