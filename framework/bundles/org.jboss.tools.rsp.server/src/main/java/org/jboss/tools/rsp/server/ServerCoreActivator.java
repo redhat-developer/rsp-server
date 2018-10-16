68255a1849d0a952821c419e66a14b962ba96793
Andre Dietisheim <adietish@redhat.com>
2018-10-16 14:33:17 +0200

    removed static context and activator variables.

--- framework/bundles/org.jboss.tools.rsp.server/src/main/java/org/jboss/tools/rsp/server/ServerCoreActivator.java
+++ framework/bundles/org.jboss.tools.rsp.server/src/main/java/org/jboss/tools/rsp/server/ServerCoreActivator.java
@@ -9,7 +9,6 @@
 package org.jboss.tools.rsp.server;
 
 import org.jboss.tools.rsp.eclipse.osgi.util.NLS;
-import org.jboss.tools.rsp.server.ShutdownExecutor.IShutdownHandler;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
@@ -17,68 +16,52 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ServerCoreActivator implements BundleActivator {
+
 	public static final String BUNDLE_ID = "org.jboss.tools.rsp.server";
 	private static final Logger LOG = LoggerFactory.getLogger(ServerCoreActivator.class);
 
-	private static BundleContext context;
-	private static ServerCoreActivator activator;
-	
-	public static BundleContext getDefaultContext() {
-		return context;
-	}
-	
-	public static ServerCoreActivator getDefault() {
-		return activator;
-	}
-	
 	@Override
-	public void start(final BundleContext context2) throws Exception {
-		activator = this;
-		context = context2;
-		setShutdownHandler();
+	public void start(final BundleContext context) throws Exception {
+		setShutdownHandler(context);
 		startServer();
 		LOG.info(NLS.bind("{0} bundle activated.", BUNDLE_ID));
 	}
-	
+
 	public ServerManagementServerLauncher getLauncher() {
 		return LauncherSingleton.getDefault().getLauncher();
 	}
-	
+
 	private int getPort() {
 		return RSPFlags.getServerPort();
 	}
-	
+
 	private void startServer() {
 		int port = getPort();
 		ServerManagementServerLauncher launcher = new ServerManagementServerLauncher();
 		LauncherSingleton.getDefault().setLauncher(launcher);
 		
-		new Thread("Launch RSP Server") {
-			public void run() {
+		new Thread(() -> {
 				try {
 					launcher.launch(port);
-				} catch( Exception e) {
+				} catch (Exception e) {
 					LOG.error("Unable to launch RSP server", e);
 				}
-			}
-		}.start();
+			}, 
+			"Launch RSP Server")
+		.start();
 	}
-	
-	private void setShutdownHandler() {
-		ShutdownExecutor.getExecutor().setHandler(new IShutdownHandler() {
-			@Override
-			public void shutdown() {
-				try {
-					context.getBundle(0).stop();
-				} catch(BundleException be) {
-					be.printStackTrace();
-				}
-			}});
+
+	private void setShutdownHandler(final BundleContext context) {
+		ShutdownExecutor.getExecutor().setHandler(() -> {
+			try {
+				context.getBundle(0).stop();
+			} catch (BundleException e) {
+				LOG.error(NLS.bind("Stopping bundle {0} failed.", BUNDLE_ID), e);
+			}
+		});
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 }
