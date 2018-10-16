e8e25490cd13935e2be7efe86518557ee857d28a
Andre Dietisheim <adietish@redhat.com>
2018-10-16 14:14:17 +0200

    removed static context var, added copyright header
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- framework/tests/org.jboss.tools.rsp.secure.test/src/org/jboss/tools/rsp/secure/test/Activator.java
+++ framework/tests/org.jboss.tools.rsp.secure.test/src/org/jboss/tools/rsp/secure/test/Activator.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
 package org.jboss.tools.rsp.secure.test;
 
 import org.osgi.framework.BundleActivator;
@@ -5,26 +13,10 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
 	}
 
 }
