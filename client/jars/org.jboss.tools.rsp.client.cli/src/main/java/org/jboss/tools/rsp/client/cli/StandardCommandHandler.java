bf8aed69fb20db8371af0ed16d73c2c41c08d459
Andre Dietisheim <adietish@redhat.com>
2018-10-19 19:49:02 +0200

    corrected small coding issues, formatting, added class header

--- client/jars/org.jboss.tools.rsp.client.cli/src/main/java/org/jboss/tools/rsp/client/cli/StandardCommandHandler.java
+++ client/jars/org.jboss.tools.rsp.client.cli/src/main/java/org/jboss/tools/rsp/client/cli/StandardCommandHandler.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
 package org.jboss.tools.rsp.client.cli;
 
 import java.util.ArrayList;
@@ -29,74 +37,73 @@ import org.jboss.tools.rsp.api.dao.util.CreateServerAttributesUtility;
 import org.jboss.tools.rsp.client.bindings.ServerManagementClientLauncher;
 
 public class StandardCommandHandler implements InputHandler {
+
 	private static final String LIST_PATHS = "list paths";
 	private static final String ADD_PATH = "add path ";
 	private static final String REMOVE_PATH = "remove path ";
 	private static final String SEARCH_PATH = "search path ";
-	
-	
+
 	private static final String LIST_SERVERS = "list servers";
 	private static final String LIST_SERVER_TYPES = "list servertypes";
 	private static final String LIST_SERVERTYPE_ATTRIBUTES_REQUIRED = "list attributes required";
 	private static final String LIST_SERVERTYPE_ATTRIBUTES_OPTIONAL = "list attributes optional";
-	
+
 	private static final String ADD_SERVER = "add server";
 	private static final String REMOVE_SERVER = "remove server ";
-	
+
 	private static final String START_SERVER = "start server ";
 	private static final String STOP_SERVER = "stop server ";
-	
+
 	private static final String LAUNCH_COMMAND = "launch command";
 	private static final String LAUNCH_LOCAL = "launch local";
-	
-	
+
 	private static final String EXIT = "exit";
 	private static final String SHUTDOWN = "shutdown";
 
 	private static final String[] CMD_ARR = new String[] {
 			LIST_PATHS, ADD_PATH, REMOVE_PATH, SEARCH_PATH, 
-			// LIST_VM, ADD_VM, REMOVE_VM,
 			LIST_SERVERS, LIST_SERVER_TYPES, 
 			LIST_SERVERTYPE_ATTRIBUTES_REQUIRED, LIST_SERVERTYPE_ATTRIBUTES_OPTIONAL,
 			ADD_SERVER, REMOVE_SERVER, 
 			LAUNCH_COMMAND, LAUNCH_LOCAL, START_SERVER, STOP_SERVER,
 			EXIT, SHUTDOWN
 	};
-	
+
 	private ServerManagementClientLauncher launcher;
 	private InputProvider provider;
 	public StandardCommandHandler(ServerManagementClientLauncher launcher, InputProvider provider) {
 		this.launcher = launcher;
 		this.provider = provider;
 	}
-	
 
 	@Override
 	public void handleInput(String line) throws Exception {
 		processCommand(line);
 	}
-	
-	
+
 	private void processCommand(String s) throws Exception {
-		if( s.trim().isEmpty())
+		if (s == null
+				|| s.isEmpty()) {
 			return;
-		if( s.trim().equals(SHUTDOWN)) {
+		}
+
+		if (s.trim().equals(SHUTDOWN)) {
 			launcher.getServerProxy().shutdown();
 			System.out.println("The server has been shutdown");
 			System.exit(0);
-		} else if( s.trim().equals(LIST_PATHS)) {
+		} else if (s.trim().equals(LIST_PATHS)) {
 			List<DiscoveryPath> list = launcher.getServerProxy().getDiscoveryPaths().get();
 			System.out.println("Paths:");
-			if( list != null ) {
+			if (list != null) {
 				for( DiscoveryPath dp : list) {
 					System.out.println("   " + dp.getFilepath());
 				}
 			}
-		} else if( s.startsWith(START_SERVER)) {
+		} else if (s.startsWith(START_SERVER)) {
 			String suffix = s.substring(START_SERVER.length()).trim();
 			String serverId = suffix;
 			ServerHandle handle = findServer(serverId);
-			if( handle == null ) {
+			if (handle == null) {
 				System.out.println("Server " + serverId + " not found.");
 			} else {
 				String mode = selectLaunchMode(handle.getType());
@@ -105,70 +112,70 @@ public class StandardCommandHandler implements InputHandler {
 				StartServerResponse stat = launcher.getServerProxy().startServerAsync(params).get();
 				System.out.println(stat.getStatus().toString());
 			}
-		} else if( s.equals(LAUNCH_COMMAND)) {
+		} else if (s.equals(LAUNCH_COMMAND)) {
 			LaunchParameters getLaunchReq = getLaunchCommandRequest();
 			if( getLaunchReq != null )
 				printLocalLaunchCommandDetails(getLaunchReq);
-		} else if( s.equals(LAUNCH_LOCAL)) {
-			runLocalLaunchScenario(s);
-		} else if( s.startsWith(STOP_SERVER)) {
+		} else if (s.equals(LAUNCH_LOCAL)) {
+			runLocalLaunchScenario();
+		} else if (s.startsWith(STOP_SERVER)) {
 			String suffix = s.substring(STOP_SERVER.length()).trim();
 			StopServerAttributes ssa = new StopServerAttributes(suffix, false);
 			Status stat = launcher.getServerProxy().stopServerAsync(ssa).get();
 			System.out.println(stat.toString());
-		} else if( s.startsWith(ADD_PATH)) {
+		} else if (s.startsWith(ADD_PATH)) {
 			String suffix = s.substring(ADD_PATH.length());
 			DiscoveryPath dp = new DiscoveryPath(suffix.trim());
 			launcher.getServerProxy().addDiscoveryPath(dp);
-		} else if( s.startsWith(REMOVE_PATH)) {
+		} else if (s.startsWith(REMOVE_PATH)) {
 			String suffix = s.substring(REMOVE_PATH.length());
 			DiscoveryPath dp = new DiscoveryPath(suffix.trim());
 			launcher.getServerProxy().removeDiscoveryPath(dp);
-		} else if( s.startsWith(SEARCH_PATH)) {
+		} else if (s.startsWith(SEARCH_PATH)) {
 			String suffix = s.substring(SEARCH_PATH.length());
 			DiscoveryPath dp = new DiscoveryPath(suffix.trim());
 			List<ServerBean> beans = launcher.getServerProxy().findServerBeans(dp).get();
 			System.out.println("Beans:");
-			if( beans != null ) {
+			if (beans != null) {
 				for( ServerBean b : beans) {
 					System.out.println("   " + b.toString());
 				}
 			}
-		} else if( s.trim().equals(LIST_SERVER_TYPES)) {
+		} else if (s.trim().equals(LIST_SERVER_TYPES)) {
 			List<ServerType> handles = launcher.getServerProxy().getServerTypes().get();
 			System.out.println(handles.size() + " servers found:");
 			for( ServerType sh : handles ) {
 				System.out.println("   " + sh.getId() + ": " + sh.getVisibleName());
 			}
 						
-		} else if( s.trim().equals(LIST_SERVERTYPE_ATTRIBUTES_REQUIRED)) {
+		} else if (s.trim().equals(LIST_SERVERTYPE_ATTRIBUTES_REQUIRED)) {
 			ServerType st = chooseServerType();
-			if( st != null ) {
+			if (st != null) {
 				Attributes attr = launcher.getServerProxy().getRequiredAttributes(st).get();
 				printAttr(attr);
 			}
-		} else if( s.trim().equals(LIST_SERVERTYPE_ATTRIBUTES_OPTIONAL)) {
+		} else if (s.trim().equals(LIST_SERVERTYPE_ATTRIBUTES_OPTIONAL)) {
 			ServerType st = chooseServerType();
 			if( st != null ) {
 				Attributes attr = launcher.getServerProxy().getOptionalAttributes(st).get();
 				printAttr(attr);
 			}
-		} else if( s.trim().equals(LIST_SERVERS)) {
+		} else if (s.trim().equals(LIST_SERVERS)) {
 			List<ServerHandle> handles = launcher.getServerProxy().getServerHandles().get();
 			System.out.println(handles.size() + " servers found:");
 			for( ServerHandle sh : handles ) {
 				System.out.println("   " + sh.getType() + ":" + sh.getId());
 			}
-		} else if( s.trim().startsWith(REMOVE_SERVER)) {
+		} else if (s.trim().startsWith(REMOVE_SERVER)) {
 			String suffix = s.substring(REMOVE_SERVER.length());
 			ServerHandle sh = findServer(suffix.trim());
-			if( sh != null )
+			if (sh != null)
 				launcher.getServerProxy().deleteServer(sh);
 			else
 				System.out.println("Server not found: " + suffix.trim());
-		} else if( s.trim().equals(ADD_SERVER)) {
+		} else if (s.trim().equals(ADD_SERVER)) {
 			runAddServer();
-		} else if( s.trim().equals(EXIT)) {
+		} else if (s.trim().equals(EXIT)) {
 			launcher.closeConnection();
 			System.exit(0);
 		} else {
@@ -179,7 +186,7 @@ public class StandardCommandHandler implements InputHandler {
 	private void printAttr(Attributes attr) {
 		Map<String, Attribute> map = attr.getAttributes();
 		Iterator<String> kit = map.keySet().iterator();
-		while(kit.hasNext()) {
+		while (kit.hasNext()) {
 			String key = kit.next();
 			Attribute val = map.get(key);
 			System.out.println(key);
@@ -189,8 +196,7 @@ public class StandardCommandHandler implements InputHandler {
 		}
 	}
 
-
-	private void runLocalLaunchScenario(String s) throws Exception {
+	private void runLocalLaunchScenario() throws Exception {
 		LaunchParameters getLaunchReq = getLaunchCommandRequest();
 		printLocalLaunchCommandDetails(getLaunchReq);
 		
@@ -209,7 +215,7 @@ public class StandardCommandHandler implements InputHandler {
 	}
 	private void printLocalLaunchCommandDetails(LaunchParameters getLaunchReq) throws Exception {
 		CommandLineDetails det = launcher.getServerProxy().getLaunchCommand(getLaunchReq).get();
-		if( det == null ) {
+		if (det == null) {
 			System.out.println("The SSP returned no launch command for this request.");
 			return;
 		}
@@ -220,6 +226,7 @@ public class StandardCommandHandler implements InputHandler {
 		System.out.println("Got it.");
 		System.out.println("command: " + String.join(" ", cmdline));
 	}
+
 	private LaunchParameters getLaunchCommandRequest() throws Exception {
 		System.out.println("Which server would you like to run?");
 		List<ServerHandle> handles = launcher.getServerProxy().getServerHandles().get();
@@ -228,7 +235,7 @@ public class StandardCommandHandler implements InputHandler {
 		}
 		String server = nextLine().trim();
 		ServerHandle handle = findServer(server);
-		if( handle == null ) {
+		if (handle == null) {
 			System.out.println("Server " + server + " not found.");
 			return null;
 		}
@@ -247,8 +254,8 @@ public class StandardCommandHandler implements InputHandler {
 
 	private ServerHandle findServer(String id) throws Exception {
 		List<ServerHandle> handles = launcher.getServerProxy().getServerHandles().get();
-		for( ServerHandle sh : handles ) {
-			if( sh.getId().equals(id)) 
+		for (ServerHandle sh : handles) {
+			if (sh.getId().equals(id))
 				return sh;
 		}
 		return null;
@@ -261,7 +268,7 @@ public class StandardCommandHandler implements InputHandler {
 			
 			System.out.println("What type of server do you want to create?");
 			int c = 1;
-			for( ServerType it : types ) {
+			for (ServerType it : types) {
 				System.out.println("   " + c + ") " + it.getId());
 				c++;
 			}
@@ -269,19 +276,19 @@ public class StandardCommandHandler implements InputHandler {
 			try {
 				Integer inte = Integer.parseInt(type);
 				int ind = inte-1;
-				if( ind >= 0 && ind < types.size() ) {
+				if (ind >= 0 && ind < types.size()) {
 					return types.get(ind);
 				}
-			} catch(NumberFormatException nfe) {
+			} catch (NumberFormatException nfe) {
 				ServerType selected = null;
-				for( ServerType st1 : types ) {
-					if( st1.getId().equals(type)) {
+				for (ServerType st1 : types) {
+					if (st1.getId().equals(type)) {
 						selected = st1;
 					}
 				}
 				return selected;
 			}
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
@@ -291,19 +298,19 @@ public class StandardCommandHandler implements InputHandler {
 
 		try {
 			ServerType selected = chooseServerType();
-			if( selected == null ) {
+			if (selected == null) {
 				System.out.println("Canceling request.");
 				return;
 			}
 			
 			System.out.println("Please choose a unique name: ");
 			String name = nextLine();
-			if( name == null || name.isEmpty()) {
+			if (name == null || name.isEmpty()) {
 				System.out.println("Name cannot be empty. Canceling request.");
 				return;
 			}
 			
-			HashMap<String, Object> store = new HashMap<String, Object>();
+			HashMap<String, Object> store = new HashMap<>();
 			Attributes required2 = launcher.getServerProxy()
 					.getRequiredAttributes(selected).get();
 			Attributes optional2 = launcher.getServerProxy()
@@ -314,7 +321,7 @@ public class StandardCommandHandler implements InputHandler {
 			System.out.println("Adding Server...");
 			ServerAttributes csa = new ServerAttributes(selected.getId(), name, store);
 			Status result = launcher.getServerProxy().createServer(csa).get();
-			if( result.isOK()) {
+			if (result.isOK()) {
 				System.out.println("Server Added");
 			} else {
 				System.out.println("Error adding server: " + result.getMessage());
@@ -326,14 +333,14 @@ public class StandardCommandHandler implements InputHandler {
 	
 	
 	private void promptForAttributes(Attributes attr, HashMap<String, Object> store, boolean required2) {
-		if( attr == null )
+		if (attr == null)
 			return;
 		
 		CreateServerAttributesUtility attrsUtil = new CreateServerAttributesUtility(attr);
 		HashMap<String, Object> toSend = store;
-		if( attrsUtil != null ) {
+		if (attrsUtil != null) {
 			Set<String> keys = attrsUtil.listAttributes();
-			for( String k : keys ) {
+			for (String k : keys) {
 				String attrType = attrsUtil.getAttributeType(k);
 				Class c = getAttributeTypeAsClass(attrType);
 				String reqType = c.getName();
@@ -343,46 +350,46 @@ public class StandardCommandHandler implements InputHandler {
 				// Workaround to sending integers over json
 				defVal = workaroundDoubles(defVal, attrType);
 				String toPrint = "Key: " + k + "\nType: " + reqType + "\nDescription: " + reqDesc;
-				if( defVal != null ) {
+				if (defVal != null) {
 					toPrint += "\nDefault Value: " + defVal.toString();
 				}
 				System.out.println(toPrint);
-				if( !required2 ) {
+				if (!required2) {
 					System.out.println("Would you like to set this value? [y/n]");
 					String val = nextLine();
-					if( val == null || val.isEmpty() || val.toLowerCase().equals("n")) {
+					if (val == null || val.isEmpty() || val.toLowerCase().equals("n")) {
 						System.out.println("Skipping");
 						continue;
 					}
 				}
 				
 				String msg = null;
-				if( Integer.class.equals(c) || Boolean.class.equals(c) || String.class.equals(c)) {
+				if (Integer.class.equals(c) || Boolean.class.equals(c) || String.class.equals(c)) {
 					msg = "Please enter a value: ";
-				} else if( List.class.equals(c)) {
+				} else if (List.class.equals(c)) {
 					msg = "Please enter a list value. Send a blank line to end the list.";
-				} else if( Map.class.equals(c)) {
+				} else if (Map.class.equals(c)) {
 					msg = "Please enter a map value. Each line should read some.key=some.val.\nSend a blank line to end the map.";
 				}
 				System.out.println(msg);
 				
-				if( Integer.class.equals(c) || Boolean.class.equals(c) || String.class.equals(c)) {
+				if (Integer.class.equals(c) || Boolean.class.equals(c) || String.class.equals(c)) {
 					String val = nextLine();
 					toSend.put(k, convertType(val, attrsUtil.getAttributeType(k)));
-				} else if( List.class.equals(c)) {
+				} else if (List.class.equals(c)) {
 					List<String> arr = new ArrayList<String>();
 					String tmp = nextLine();
-					while(!tmp.trim().isEmpty()) {
+					while (!tmp.trim().isEmpty()) {
 						arr.add(tmp);
 						tmp = nextLine();
 					}
 					toSend.put(k, arr);
-				} else if( Map.class.equals(c)) {
-					Map<String, String> map = new HashMap<String, String>();
+				} else if (Map.class.equals(c)) {
+					Map<String, String> map = new HashMap<>();
 					String tmp = nextLine();
-					while(!tmp.trim().isEmpty()) {
+					while (!tmp.trim().isEmpty()) {
 						int ind = tmp.indexOf("=");
-						if( ind == -1 ) {
+						if (ind == -1) {
 							System.out.println("Invalid map entry. Please try again");
 						} else {
 							String k1 = tmp.substring(0,  ind);
@@ -400,34 +407,34 @@ public class StandardCommandHandler implements InputHandler {
 	private Object workaroundDoubles(Object defaultVal, String attrType) {
 
 		// Workaround for the problems with json transfer
-		Class intended = getAttributeTypeAsClass(attrType);
-		if( Integer.class.equals(intended) && Double.class.equals(defaultVal.getClass())) {
-			return new Integer(((Double)defaultVal).intValue());
+		Class<?> intended = getAttributeTypeAsClass(attrType);
+		if (Integer.class.equals(intended) && Double.class.equals(defaultVal.getClass())) {
+			return Integer.valueOf(((Double)defaultVal).intValue());
 		}
 		return defaultVal;
 	}
 	
 	private Class getAttributeTypeAsClass(String type) {
-		if( ServerManagementAPIConstants.ATTR_TYPE_INT.equals(type)) {
+		if (ServerManagementAPIConstants.ATTR_TYPE_INT.equals(type)) {
 			return Integer.class;
-		} else if( ServerManagementAPIConstants.ATTR_TYPE_BOOL.equals(type)) {
+		} else if (ServerManagementAPIConstants.ATTR_TYPE_BOOL.equals(type)) {
 			return Boolean.class;
-		} else if( ServerManagementAPIConstants.ATTR_TYPE_STRING.equals(type)) {
+		} else if (ServerManagementAPIConstants.ATTR_TYPE_STRING.equals(type)) {
 			return String.class;
-		} else if( ServerManagementAPIConstants.ATTR_TYPE_LIST.equals(type)) {
+		} else if (ServerManagementAPIConstants.ATTR_TYPE_LIST.equals(type)) {
 			return List.class;
-		} else if( ServerManagementAPIConstants.ATTR_TYPE_MAP.equals(type)) {
+		} else if (ServerManagementAPIConstants.ATTR_TYPE_MAP.equals(type)) {
 			return Map.class;
 		}
 		return null;
 	}
 	
 	private Object convertType(String input, String type) {
-		if( ServerManagementAPIConstants.ATTR_TYPE_INT.equals(type)) {
+		if (ServerManagementAPIConstants.ATTR_TYPE_INT.equals(type)) {
 			return Integer.parseInt(input);
-		} else if(ServerManagementAPIConstants.ATTR_TYPE_STRING.equals(type)) {
+		} else if (ServerManagementAPIConstants.ATTR_TYPE_STRING.equals(type)) {
 			return input;
-		} else if( ServerManagementAPIConstants.ATTR_TYPE_BOOL.equals(type)) {
+		} else if (ServerManagementAPIConstants.ATTR_TYPE_BOOL.equals(type)) {
 			return Boolean.parseBoolean(input);
 		}
 		return null;
@@ -436,17 +443,16 @@ public class StandardCommandHandler implements InputHandler {
 	private void showCommands() {
 		System.out.println("Invalid Command");
 		System.out.println("Possible commands: ");
-		for( int i = 0; i < CMD_ARR.length; i++ ) {
+		for (int i = 0; i < CMD_ARR.length; i++) {
 			System.out.println("   " + CMD_ARR[i]);
 		}
-		
 	}
 	
 	private String selectLaunchMode(ServerType st) throws Exception {
 		List<ServerLaunchMode> modes = launcher.getServerProxy().getLaunchModes(st).get();
-		if( modes.size() > 0 ) {
+		if (!modes.isEmpty()) {
 			System.out.println("Please select a launch mode:");
-			for( ServerLaunchMode slm : modes ) {
+			for (ServerLaunchMode slm : modes) {
 				System.out.println(slm.getMode());
 			}
 		}
@@ -454,11 +460,10 @@ public class StandardCommandHandler implements InputHandler {
 		return mode;
 	}
 
-	
 	public String nextLine() {
 		return nextLine(null);
 	}
-	
+
 	public String nextLine(String prompt) {
 		String p = (prompt == null ? "" : prompt);
 		StandardPrompt sp = new StandardPrompt(p);
@@ -466,6 +471,7 @@ public class StandardCommandHandler implements InputHandler {
 		sp.await();
 		return sp.ret;
 	}
+
 	private static class StandardPrompt implements InputHandler {
 		private String prompt;
 		private String ret;
@@ -483,20 +489,18 @@ public class StandardCommandHandler implements InputHandler {
 			this.ret = line;
 			doneSignal.countDown();
 		}
-		
+
 		public void await() {
 			try {
 				doneSignal.await();
-			} catch(InterruptedException ie) {
+			} catch (InterruptedException ie) {
 				ie.printStackTrace();
 			}
 		}
-		
 	}
 
 	@Override
 	public String getPrompt() {
 		return "Please enter a command.\n";
 	}
-
 }
