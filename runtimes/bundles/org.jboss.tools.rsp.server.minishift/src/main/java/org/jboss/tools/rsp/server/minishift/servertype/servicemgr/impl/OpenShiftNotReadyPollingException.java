8f89d9368b08906746b1e09b8402b53ca95dcd38
Andre Dietisheim <adietish@redhat.com>
2018-10-26 14:10:10 +0200

    removed servicemanagerenv, added class header to NotReadyException
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- runtimes/bundles/org.jboss.tools.rsp.server.minishift/src/main/java/org/jboss/tools/rsp/server/minishift/servertype/servicemgr/impl/OpenShiftNotReadyPollingException.java
+++ runtimes/bundles/org.jboss.tools.rsp.server.minishift/src/main/java/org/jboss/tools/rsp/server/minishift/servertype/servicemgr/impl/OpenShiftNotReadyPollingException.java
@@ -1,11 +1,22 @@
+/******************************************************************************* 
+ * Copyright (c) 2018 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
 package org.jboss.tools.rsp.server.minishift.servertype.servicemgr.impl;
 
 import org.jboss.tools.rsp.eclipse.core.runtime.IStatus;
 import org.jboss.tools.rsp.server.spi.model.polling.PollingException;
 
 public class OpenShiftNotReadyPollingException extends PollingException {
+
 	public static final int OPENSHIFT_UNREACHABLE_CODE = 10001;
-	private transient IStatus stat;
+	private final transient IStatus stat;
 
 	public OpenShiftNotReadyPollingException(IStatus status) {
 		super(status.getMessage());
