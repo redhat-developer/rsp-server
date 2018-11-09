8e2292b612a439356d76dc748793b8bd8d81a44d
Andre Dietisheim <adietish@redhat.com>
2018-11-09 12:44:49 +0100

    added copyright header, removed generated comments
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- framework/bundles/org.jboss.tools.rsp.server.spi/src/main/java/org/jboss/tools/rsp/server/spi/model/ServerModelListenerAdapter.java
+++ framework/bundles/org.jboss.tools.rsp.server.spi/src/main/java/org/jboss/tools/rsp/server/spi/model/ServerModelListenerAdapter.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
 package org.jboss.tools.rsp.server.spi.model;
 
 import org.jboss.tools.rsp.api.dao.ServerHandle;
@@ -7,44 +15,30 @@ public class ServerModelListenerAdapter implements IServerModelListener {
 
 	@Override
 	public void serverAdded(ServerHandle server) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void serverRemoved(ServerHandle server) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void serverAttributesChanged(ServerHandle server) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void serverStateChanged(ServerHandle server, ServerState state) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void serverProcessCreated(ServerHandle server, String processId) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void serverProcessTerminated(ServerHandle server, String processId) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void serverProcessOutputAppended(ServerHandle server, String processId, int streamType, String text) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
