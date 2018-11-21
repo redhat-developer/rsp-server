a260ee168aad21c2f1664201555e44adc2c8107a
Rob Stryker <rob@oxbeef.net>
2018-11-21 11:46:06 -0500

    Migrating JBoss Stacks plugin to pure osgi
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ framework/bundles/org.jboss.tools.rsp.stacks.core/src/org/jboss/tools/rsp/stacks/core/model/StacksManager.java
@@ -0,0 +1,293 @@
+/*************************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
+package org.jboss.tools.rsp.stacks.core.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.jboss.jdf.stacks.client.DefaultStacksClientConfiguration;
+import org.jboss.jdf.stacks.client.StacksClient;
+import org.jboss.jdf.stacks.client.StacksClientConfiguration;
+import org.jboss.jdf.stacks.client.messages.StacksMessages;
+import org.jboss.jdf.stacks.model.Stacks;
+import org.jboss.jdf.stacks.parser.Parser;
+import org.jboss.tools.rsp.eclipse.core.runtime.CoreException;
+import org.jboss.tools.rsp.eclipse.core.runtime.IProgressMonitor;
+import org.jboss.tools.rsp.eclipse.core.runtime.Path;
+import org.jboss.tools.rsp.eclipse.core.runtime.SubProgressMonitor;
+import org.jboss.tools.rsp.launching.LaunchingCore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A StacksManager is in charge of retrieving a file from a URL or standard
+ * location and returning a jdf.stacks model object generated via the stacks
+ * client.
+ */
+public class StacksManager {
+
+	private static final Logger LOG = LoggerFactory.getLogger(StacksManager.class);
+
+	/**
+	 * @deprecated
+	 */
+	private static final String STACKS_URL_PROPERTY = "org.jboss.examples.stacks.url";
+
+	private static final String URL_PROPERTY_STACKS = "org.jboss.tools.stacks.url_stacks";
+	private static final String URL_PROPERTY_PRESTACKS = "org.jboss.tools.stacks.url_prestacks";
+
+	private static final String STACKS_URL;
+	private static final String PRESTACKS_URL;
+
+	// Declare the types of stacks available for fetch
+	public enum StacksType {
+		STACKS_TYPE, PRESTACKS_TYPE
+	}
+
+	// Load the default stacks url and prestacks url from a sysprop or jar
+	static {
+		STACKS_URL = System.getProperty(URL_PROPERTY_STACKS, System.getProperty(STACKS_URL_PROPERTY,
+				System.getProperty(StacksClientConfiguration.REPO_PROPERTY, getStacksDefaultUrlFromJar())));
+		PRESTACKS_URL = System.getProperty(URL_PROPERTY_PRESTACKS,
+				System.getProperty("jdf.prestacks.client.repo", getPreStacksDefaultUrlFromJar()));
+	}
+
+	/**
+	 * Fetch the default stacks model.
+	 * 
+	 * @param monitor
+	 * @return
+	 */
+	public Stacks getStacks(IProgressMonitor monitor) {
+		Stacks[] all = getStacks("Fetching JBoss Stacks", monitor, StacksType.STACKS_TYPE);
+		if (all != null && all.length > 0)
+			return all[0];
+		return null;
+	}
+
+	/**
+	 * Fetch an array of stacks models where each element represents one of the
+	 * StacksType urls
+	 * 
+	 * @param jobName
+	 * @param monitor
+	 * @param types
+	 * @return
+	 */
+	public Stacks[] getStacks(String jobName, IProgressMonitor monitor, StacksType... types) {
+		if (types == null)
+			return new Stacks[0];
+		LOG.trace("Request received for " + types.length + " stacks types.");
+		ArrayList<Stacks> ret = new ArrayList<Stacks>(types.length);
+		monitor.beginTask(jobName, types.length * 100);
+		for (int i = 0; i < types.length; i++) {
+			switch (types[i]) {
+			case STACKS_TYPE:
+				LOG.trace("Loading Stacks Model from " + STACKS_URL);
+				Stacks s = getStacks(STACKS_URL, jobName, new SubProgressMonitor(monitor, 50));
+				if (s == null && !monitor.isCanceled()) {
+					LOG.warn("Stacks from " + STACKS_URL + " can not be read, using client mechanism instead");
+					s = getDefaultStacksFromClient(new SubProgressMonitor(monitor, 50));
+				}
+				if (s != null)
+					ret.add(s);
+				break;
+			case PRESTACKS_TYPE:
+				// Pre-stacks has no fall-back mechanism at this time
+				LOG.trace("Loading Stacks Model from " + PRESTACKS_URL);
+				Stacks s2 = getStacks(PRESTACKS_URL, jobName, new SubProgressMonitor(monitor, 100));
+				if (s2 != null)
+					ret.add(s2);
+				break;
+			default:
+				break;
+			}
+		}
+		monitor.done();
+		return (Stacks[]) ret.toArray(new Stacks[ret.size()]);
+	}
+
+	/**
+	 * Fetch the stacks model representing a given arbitrary url. The remote file
+	 * will be cached only until the system exits.
+	 * 
+	 * @param url
+	 * @param monitor
+	 * @return
+	 */
+	public Stacks getStacks(String url, IProgressMonitor monitor) {
+		return getStacksFromURL(url, url, monitor);
+	}
+
+	/**
+	 * Fetch the stacks model for a given url. Cache the remote file with a duration
+	 * representing forever, or, until the remote file is newer.
+	 * 
+	 * @param url     The url
+	 * @param jobName Job name for display purposes
+	 * @param monitor
+	 * @return
+	 */
+	protected Stacks getStacks(String url, String jobName, IProgressMonitor monitor) {
+		return getStacksFromURL(url, jobName, monitor);
+	}
+
+	protected Stacks getStacksFromURL(String url, String jobName, IProgressMonitor monitor) {
+
+		Stacks stacks = null;
+		try {
+			LOG.trace("Locating or downloading file for " + url);
+			File f = getCachedFileForURL(url, jobName, monitor);
+			return getStacksFromFile(f);
+		} catch (Exception e) {
+			LOG.error("Can't access or parse  " + url, e); //$NON-NLS-1$
+		}
+		return stacks;
+	}
+
+	protected Stacks getStacksFromFile(File f) throws IOException {
+		if (f != null && f.exists()) {
+			LOG.trace("Local file for url exists");
+			FileInputStream fis = null;
+			try {
+				fis = new FileInputStream(f);
+				Parser p = new Parser();
+				return p.parse(fis);
+			} finally {
+				close(fis);
+			}
+		}
+		return null;
+	}
+
+	private Stacks getDefaultStacksFromClient(IProgressMonitor monitor) {
+		if (!monitor.isCanceled()) {
+			final StacksClient client = new StacksClient(new DefaultStacksClientConfiguration(),
+					new JBTStacksMessages());
+			return runWithTimeout(5000, () -> client.getStacks());
+		}
+		return null;
+	}
+
+	public static <R> R runWithTimeout(long millisTimeout, Callable<R> callable) {
+		ExecutorService singleThreadExecutor = Executors.newFixedThreadPool(1);
+		Future<R> future = singleThreadExecutor.submit(callable);
+		try {
+			return future.get(millisTimeout, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+		} finally {
+			singleThreadExecutor.shutdown();
+		}
+		return null;
+	}
+
+	/**
+	 * Fetch a local cache of the remote file. If the remote file is newer than the
+	 * local, update it.
+	 * 
+	 * @param url     A url to fetch the stacks model from
+	 * @param jobName A job name passed into the downloader for display purposes
+	 * @param monitor A file representign the model
+	 * @return
+	 */
+	protected File getCachedFileForURL(String url, String jobName, IProgressMonitor monitor) throws CoreException {
+		if( getCache().isCacheOutdated(url, monitor)) {
+			return cache.downloadAndCache(url,jobName, 10000, true, monitor);
+		} else {
+			// Else use the local cache
+			return cache.getCachedFile(url);
+		}
+	}
+	
+	private URLTransportCache cache;
+	private URLTransportCache getCache() {
+		if( cache == null ) {
+			File data = LaunchingCore.getDataLocation();
+			File stacks = new File(data, "stacks");
+			cache = new URLTransportCache(new Path(stacks.getAbsolutePath()));
+		}
+		return cache;
+	}
+
+	/*
+	 * Read the stacks.yaml location from inside our client jar
+	 */
+	private static String getStacksDefaultUrlFromJar() {
+		return getUrlFromJar(StacksClientConfiguration.REPO_PROPERTY);
+	}
+
+	private static String getPreStacksDefaultUrlFromJar() {
+		return getUrlFromJar(StacksClientConfiguration.PRESTACKS_REPO_PROPERTY);
+	}
+
+	private static String getUrlFromJar(String prop) {
+		InputStream is = null;
+		try {
+			is = StacksManager.class.getResourceAsStream("/org/jboss/jdf/stacks/client/config.properties"); //$NON-NLS-1$
+			Properties p = new Properties();
+			p.load(is);
+			return p.getProperty(prop);
+		} catch (Exception e) {
+			LOG.warn("Can't read stacks url from the stacks-client.jar", e); //$NON-NLS-1$
+		} finally {
+			close(is);
+		}
+		return null;
+	}
+
+	private static class JBTStacksMessages implements StacksMessages {
+		public void showDebugMessage(String arg0) {
+			LOG.trace(arg0);
+		}
+
+		public void showInfoMessage(String arg0) {
+			LOG.info(arg0);
+		}
+
+		public void showErrorMessage(String arg0) {
+			LOG.error(arg0);
+		}
+
+		public void showErrorMessageWithCause(String arg0, Throwable t) {
+			LOG.error(arg0, t);
+		}
+
+		public void showWarnMessage(String arg0) {
+			LOG.warn(arg0);
+		}
+
+	}
+
+	/*
+	 * Close an inputstream
+	 */
+	private static void close(InputStream is) {
+		if (is != null) {
+			try {
+				is.close();
+			} catch (IOException ie) {
+				// IGNORE
+			}
+		}
+	}
+
+}
---
+++ framework/bundles/org.jboss.tools.rsp.stacks.core/src/org/jboss/tools/rsp/stacks/core/model/URLTransportCache.java
@@ -0,0 +1,367 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.rsp.stacks.core.model;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.jboss.tools.rsp.eclipse.core.runtime.CoreException;
+import org.jboss.tools.rsp.eclipse.core.runtime.IPath;
+import org.jboss.tools.rsp.eclipse.core.runtime.IProgressMonitor;
+import org.jboss.tools.rsp.eclipse.core.runtime.IStatus;
+import org.jboss.tools.rsp.eclipse.core.runtime.Status;
+import org.jboss.tools.rsp.stacks.core.StacksCoreActivator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class URLTransportCache {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(URLTransportCache.class);
+
+	
+	/**
+	 * Encoding for this file
+	 */
+	private static final String ENCODING = "UTF-8";
+	
+	
+	/**
+	 * The new index file, to be stored in each cache root folder. 
+	 */
+	private static final String CACHE_INDEX_FILE = "URLTransportCache.cacheIndex.properties";
+	
+	
+	/**
+	 * A collection of all caches currently in use. 
+	 * This is to ensure multiple clients don't try using 
+	 * two instances with the same basedir, which could lead to corruption of the cache.
+	 */
+	private static final HashMap<IPath, URLTransportCache> cacheDirToCache = new HashMap<IPath, URLTransportCache>();
+
+	public synchronized static URLTransportCache getCache(IPath root) {
+		URLTransportCache c = cacheDirToCache.get(root);
+		if( c == null ) {
+			c = new URLTransportCache(root);
+			cacheDirToCache.put(root, c);
+		}
+		return c;
+	}
+	
+	
+	private HashMap<String, String> cache;
+	private IPath cacheRoot;
+	protected URLTransportCache(IPath cacheRoot) {
+		this.cacheRoot = cacheRoot;
+		this.cache = new HashMap<String, String>();
+		load();
+	}
+
+	/**
+	 * Get a cached file for the given url only if it is downloaded and exists.
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public File getCachedFile(String url) {
+		String cacheVal = cache.get(url);
+		if (cacheVal == null)
+			return null;
+		File f = new File(cacheVal);
+		if (f.exists())
+			return f;
+		return null;
+	}
+
+	/**
+	 * Check whether the cache is outdated
+	 * @throws CoreException if the remote url is invalid, or the remote url cannot be reached
+	 */
+	public boolean isCacheOutdated(String url, IProgressMonitor monitor)
+			throws CoreException {
+		LOG.trace("Checking if cache is outdated for " + url);
+		File f = getCachedFile(url);
+		if (f == null)
+			return true;
+
+		URL url2 = null;
+		try {
+			url2 = new URL(url);
+		} catch (MalformedURLException murle) {
+			throw new CoreException(new Status(IStatus.ERROR, StacksCoreActivator.PLUGIN_ID, murle.getMessage(), murle));
+		}
+
+		long remoteModified = getLastModified(url2, monitor);
+
+		// If the remoteModified is -1 but we have a local cache, use that (not outdated)
+		if (remoteModified == -1 ) {
+			if (f.exists())
+				return false;
+		}
+		// !!! urlModified == 0 when querying files from github
+		// It means that files from github can not be cached!
+		if (f.exists()) {
+			long modified = f.lastModified();
+			if( remoteModified > modified ) {
+				// The remote file has been updated *after* the local file was created, so, outdated
+				return true;
+			}
+			if( remoteModified == 0 ) {
+				// File comes from github or some other server not keeping accurate timestamps
+				// so, possibly oudated, and must re-fetch 
+				return true;
+			}
+			// Our local copy has a higher timestamp, so was fetched after
+			return false;
+		}
+		// Local file doesn't exist, so, cache is outdated
+		return true;
+	}
+
+	public File downloadAndCache(String url, String displayName,
+			IProgressMonitor monitor) throws CoreException {
+		return downloadAndCache(url, displayName, -1, false, monitor);
+	}
+	
+	public File downloadAndCache(String url, String displayName, 
+			int timeout, boolean deleteOnExit, IProgressMonitor monitor) throws CoreException {
+
+		LOG.trace("Downloading and caching " + url);
+
+		File existing = getExistingRemoteFileCacheLocation(url);
+		File target = createNewRemoteFileCacheLocation(url);
+		try {
+			FileOutputStream os = new FileOutputStream(target);
+			IStatus s = download(displayName, url, os, timeout, monitor);
+			if (s.isOK()) {
+				// Download completed successfully, add to cache, delete old copy
+				if (deleteOnExit)
+					target.deleteOnExit();	
+				addToCache(url, target);
+				if( existing != null && existing.exists())
+					existing.delete();
+				return target != null && target.exists() ? target : null;
+			}
+			// Download did not go as planned. Delete the new, return the old
+			if( target != null && target.exists()) {
+				target.delete();
+			}
+			return existing;
+		} catch (IOException ioe) {
+			LOG.error(ioe.getMessage(), ioe);
+		}
+		return null;
+	}
+
+	private void addToCache(String url, File target) {
+		cache.put(url, target.getAbsolutePath());
+		savePreferences();
+	}
+	
+	private void load() {
+		File index = cacheRoot.append(CACHE_INDEX_FILE).toFile();
+		if( index.exists() && index.isFile()) {
+			try {
+				String contents = getContents(index);
+				loadIndexFromString(contents);
+			} catch(IOException ioe) {
+				LOG.error(ioe.getMessage(), ioe);
+			}
+		}
+		LOG.trace("Loaded " + cache.size() + " cache file locations from preferences");
+	}
+	
+	private void loadIndexFromString(String val) {
+		if( !isEmpty(val)) {
+			String[] byLine = val.split("\n");
+			for( int i = 0; i < byLine.length; i++ ) {
+				if( isEmpty(byLine[i]))
+					continue;
+				String[] kv = byLine[i].split("=");
+				if( kv.length == 2 && !isEmpty(kv[0]) && !isEmpty(kv[1])) {
+					try {
+						String decodedUrl = URLDecoder.decode(kv[0],ENCODING);
+						if( new File(kv[1]).exists() )
+							cache.put(decodedUrl,kv[1]);
+					} catch(UnsupportedEncodingException uee) {
+						// Should not be hit
+						LOG.error(uee.getMessage(), uee);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	private boolean isEmpty(String s) {
+		return s == null || "".equals(s);
+	}
+	
+	private void savePreferences() {
+		// Saves are now done to an index file in the cache root. 
+		File index = cacheRoot.append(CACHE_INDEX_FILE).toFile();
+		
+		LOG.trace("Saving " + cache.size() + " cache file locations to " + index.getAbsolutePath());
+
+		StringBuffer sb = new StringBuffer();
+		Iterator<String> it = cache.keySet().iterator();
+		while(it.hasNext()) {
+			String k = it.next();
+			String v = cache.get(k);
+			String encodedURL = null;
+			try {
+				encodedURL = URLEncoder.encode(k, ENCODING);
+			} catch(UnsupportedEncodingException uee) {
+				// Should never happen
+			}
+			if( encodedURL != null )
+				sb.append(encodedURL + "=" + v + "\n");
+		}
+
+		try {
+			setContents(index, sb.toString());
+		} catch (IOException e) {
+			LOG.error(e.getMessage(), e);
+		}
+	}
+
+	/*
+	 * Get the existing cache location for the given url if it exists 
+	 * @param url
+	 * @return
+	 */
+	private synchronized File getExistingRemoteFileCacheLocation(String url) {
+		// If this url is already cached, use it
+		String cachedLoc = cache.get(url);
+		if (cachedLoc != null) {
+			File f = new File(cache.get(url));
+			return f;
+		}
+		return null;
+	}	
+	
+	private synchronized File createNewRemoteFileCacheLocation(String url) {
+		// Otherwise, make a new one
+		File root = getLocalCacheFolder().toFile();
+		root.mkdirs();
+		String tmp;
+		try {
+			tmp = DigestUtils.sha1(url);
+		} catch (IOException O_o) {
+		  //That really can't happen
+			tmp = url.replaceAll("[\\p{Punct}&&[^_]]", "_");
+		}
+		
+		File cached = null;
+		do {
+			cached = new File(root, 
+					tmp + new SecureRandom().nextLong() + ".tmp");
+		} while (cached.exists());
+
+		return cached;
+	}
+	
+	private IPath getLocalCacheFolder() {
+		return cacheRoot;
+	}
+	
+	/* 
+	 * foundation.core has no IO utility classes. 
+	 * If it gets some, this should be saved there. 
+	 */
+	
+	private static String getContents(File aFile) throws IOException {
+		return new String(getBytesFromFile(aFile), ENCODING);
+	}
+
+	private static byte[] getBytesFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
+        try {
+	        byte[] bytes = new byte[(int)file.length()];
+	        int offset = 0;
+	        int numRead = 0;
+	        while (offset < bytes.length
+	               && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+	            offset += numRead;
+	        }
+	        return bytes;
+        } finally {
+            is.close();
+        }
+    }
+	
+	private static void setContents(File file, String contents) throws IOException {
+		Writer out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(file), ENCODING));
+			out.append(contents);
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+	
+	private long getLastModified(URL url2, IProgressMonitor monitor) {
+		try {
+			HttpURLConnection con = (HttpURLConnection) url2.openConnection();
+			try (AutoCloseable conc = () -> con.disconnect()) {
+				return con.getLastModified();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return -1;
+	}
+	private IStatus download(String displayName, String url, FileOutputStream fileOutputStream, int timeout, IProgressMonitor monitor) throws MalformedURLException, IOException {
+		// TODO respect timeout
+		ReadableByteChannel readableByteChannel = null;
+		FileChannel fileChannel = null;
+		try {
+			readableByteChannel = Channels.newChannel(new URL(url).openStream());
+			fileChannel = fileOutputStream.getChannel();
+			fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+			return Status.OK_STATUS;
+		} finally {
+	        if (readableByteChannel != null) {
+	        	readableByteChannel.close();
+	        }
+	        if (fileChannel != null) {
+	        	fileChannel.close();
+	        }
+		}
+	}
+
+
+	
+}
