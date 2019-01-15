50239534bad3b580762e980f038a8e0c5b879599
Andre Dietisheim <adietish@redhat.com>
2019-01-15 17:18:43 +0100

    extracted constants, cleaned code
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- distribution/integration-tests/src/test/java/org/jboss/tools/rsp/itests/wildfly/WildFlyLaunchingTest.java
+++ distribution/integration-tests/src/test/java/org/jboss/tools/rsp/itests/wildfly/WildFlyLaunchingTest.java
@@ -34,127 +34,134 @@ import static org.junit.Assert.*;
  */
 public class WildFlyLaunchingTest extends RSPCase {
     
-    private final DummyClient client = launcher.getClient();
-    
-    @Test
+    private static final String MODE_DEBUG = "debug";
+	private static final String MODE_RUN = "run";
+	private static final String STATUS_MESSAGE_OK = "ok";
+	private static final String STATE_STOPPED = "stopped";
+	private static final String STATE_STARTED = "started";
+	private static final long WAIT_FOR_SERVERSTATE = 1000;
+
+	private final DummyClient client = launcher.getClient();
+
+	@Test
     public void testGetLaunchModes() throws Exception {
         List<ServerLaunchMode> modes = serverProxy.getLaunchModes(wfly13Type).get();
         
-        ServerLaunchMode expectedRun = new ServerLaunchMode("run", "A launch mode indicating a simple run.");
-        ServerLaunchMode expectedDebug = new ServerLaunchMode("debug",
+        ServerLaunchMode expectedRun = new ServerLaunchMode(MODE_RUN, "A launch mode indicating a simple run.");
+        ServerLaunchMode expectedDebug = new ServerLaunchMode(MODE_DEBUG,
             "A launch mode indicating a debug launch, which can add the appropriate debugging flags or system properties as required.");
         
         assertTrue(modes.contains(expectedRun));
         assertTrue(modes.contains(expectedDebug));
     }
-    
+
     @Test
     public void testGetLaunchModesInvalid() throws Exception {
         List<ServerLaunchMode> modes = serverProxy.getLaunchModes(new ServerType("foo", "foo", "foo")).get();
         
         assertNull(modes);
     }
-    
+
     @Test
     public void testGetLaunchModesNull() throws Exception {
         List<ServerLaunchMode> modes = serverProxy.getLaunchModes(null).get();
         
         assertNull(modes);
     }
-    
+
     @Test
     public void testGetRequiredLaunchAttributes() throws Exception {
-        LaunchAttributesRequest req = new LaunchAttributesRequest(wfly13Type.getId(), "run");
+        LaunchAttributesRequest req = new LaunchAttributesRequest(wfly13Type.getId(), MODE_RUN);
         Attributes attr = serverProxy.getRequiredLaunchAttributes(req).get();
         
         assertNotNull(attr);
     }
-    
+
     @Test
     public void testGetRequiredLaunchAttributesInvalid() throws Exception {
-        LaunchAttributesRequest req = new LaunchAttributesRequest("foo", "run");
+        LaunchAttributesRequest req = new LaunchAttributesRequest("foo", MODE_RUN);
         Attributes attr = serverProxy.getRequiredLaunchAttributes(req).get();
         
         assertNull(attr);
     }
-    
+
     @Test
     public void testGetRequiredLaunchAttributesNull() throws Exception {
         Attributes attr = serverProxy.getRequiredLaunchAttributes(null).get();
         
         assertNull(attr);
     }
-    
+
     @Test
     public void testGetOptionalLaunchAttributes() throws Exception {
-        LaunchAttributesRequest req = new LaunchAttributesRequest(wfly13Type.getId(), "run");
+        LaunchAttributesRequest req = new LaunchAttributesRequest(wfly13Type.getId(), MODE_RUN);
         Attributes attr = serverProxy.getOptionalLaunchAttributes(req).get();
         
         assertNotNull(attr);
     }
-    
+
     @Test
     public void testGetOptionalLaunchAttributesInvalid() throws Exception {
-        LaunchAttributesRequest req = new LaunchAttributesRequest("foo", "run");
+        LaunchAttributesRequest req = new LaunchAttributesRequest("foo", MODE_RUN);
         Attributes attr = serverProxy.getOptionalLaunchAttributes(req).get();
         
         assertNull(attr);
     }
-    
+
     @Test
     public void testGetOptionalLaunchAttributesNull() throws Exception {
         Attributes attr = serverProxy.getOptionalLaunchAttributes(null).get();
         
         assertNull(attr);
     }
-    
+
     @Test
     public void testGetLaunchCommand() throws Exception {
         createServer(WILDFLY13_ROOT, "wildfly");
         
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes(wfly13Type.getId(), "wildfly", new HashMap<>()), "run");
+                new ServerAttributes(wfly13Type.getId(), "wildfly", new HashMap<>()), MODE_RUN);
         
         CommandLineDetails cmd = serverProxy.getLaunchCommand(params).get();
         
         assertFalse(cmd.getWorkingDir().isEmpty());
         assertTrue(cmd.getCmdLine().length > 0);
     }
-    
+
     @Test
     public void testGetLaunchCommandInvalid() throws Exception {
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes("foo", "bar", new HashMap<>()), "run");
+                new ServerAttributes("foo", "bar", new HashMap<>()), MODE_RUN);
         
         CommandLineDetails cmd = serverProxy.getLaunchCommand(params).get();
         
         assertNull(cmd);
     }
-    
+
     @Test
     public void testGetLaunchCommandNull() throws Exception {
         CommandLineDetails cmd = serverProxy.getLaunchCommand(null).get();
         
         assertNull(cmd);
     }
-    
+
     @Test
     public void testServerStartingByClient() throws Exception {
         createServer(WILDFLY13_ROOT, "wildfly1");
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes(wfly13Type.getId(), "wildfly1", new HashMap<>()), "run");
+                new ServerAttributes(wfly13Type.getId(), "wildfly1", new HashMap<>()), MODE_RUN);
         ServerStartingAttributes attr = new ServerStartingAttributes(params, false);
         
         Status status = serverProxy.serverStartingByClient(attr).get();
         
         assertEquals(0, status.getSeverity());
-        assertEquals("ok", status.getMessage());
+        assertEquals(STATUS_MESSAGE_OK, status.getMessage());
     }
-    
+
     @Test
     public void testServerStartingByClientInvalid() throws Exception {
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes("foo", "bar", new HashMap<>()), "run");
+                new ServerAttributes("foo", "bar", new HashMap<>()), MODE_RUN);
         ServerStartingAttributes attr = new ServerStartingAttributes(params, false);
         
         Status status = serverProxy.serverStartingByClient(attr).get();
@@ -162,7 +169,7 @@ public class WildFlyLaunchingTest extends RSPCase {
         assertEquals(Status.ERROR, status.getSeverity());
         assertEquals("Server bar does not exist", status.getMessage());
     }
-    
+
     @Test
     public void testServerStartingByClientNull() throws Exception {
         Status status = serverProxy.serverStartingByClient(null).get();
@@ -170,30 +177,30 @@ public class WildFlyLaunchingTest extends RSPCase {
         assertEquals(Status.ERROR, status.getSeverity());
         assertEquals(INVALID_PARAM, status.getMessage());
     }
-    
+
     @Test
     public void testServerStartedByClient() throws Exception {
         createServer(WILDFLY13_ROOT, "wildfly2");
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes(wfly13Type.getId(), "wildfly2", new HashMap<>()), "run");
+                new ServerAttributes(wfly13Type.getId(), "wildfly2", new HashMap<>()), MODE_RUN);
         
         Status status = serverProxy.serverStartedByClient(params).get();
         
         assertEquals(0, status.getSeverity());
-        assertEquals("ok", status.getMessage());
+        assertEquals(STATUS_MESSAGE_OK, status.getMessage());
     }
-    
+
     @Test
     public void testServerStartedByClientInvalid() throws Exception {
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes("foo", "bar", new HashMap<>()), "run");
+                new ServerAttributes("foo", "bar", new HashMap<>()), MODE_RUN);
         
         Status status = serverProxy.serverStartedByClient(params).get();
         
         assertEquals(Status.ERROR, status.getSeverity());
         assertEquals("Server bar does not exist", status.getMessage());
     }
-    
+
     @Test
     public void testServerStartedByClientNull() throws Exception {
         Status status = serverProxy.serverStartedByClient(null).get();
@@ -201,11 +208,10 @@ public class WildFlyLaunchingTest extends RSPCase {
         assertEquals(Status.ERROR, status.getSeverity());
         assertEquals(INVALID_PARAM, status.getMessage());
     }
-    
-    
+
     @Test
     public void testStartServerInvalid() throws Exception {
-        LaunchParameters params = new LaunchParameters(new ServerAttributes("foo", "foo", new HashMap<>()), "run");
+        LaunchParameters params = new LaunchParameters(new ServerAttributes("foo", "foo", new HashMap<>()), MODE_RUN);
         StartServerResponse response = serverProxy.startServerAsync(params).get();
         
         assertEquals(Status.ERROR, response.getStatus().getSeverity());
@@ -226,62 +232,66 @@ public class WildFlyLaunchingTest extends RSPCase {
         Map<String, Object> attr = new HashMap<>();
         attr.put("server.home.dir", WILDFLY13_ROOT);
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes(wfly13Type.getId(), "wildfly3", attr), "run");
+                new ServerAttributes(wfly13Type.getId(), "wildfly3", attr), MODE_RUN);
         
         StartServerResponse response = serverProxy.startServerAsync(params).get();
         
         assertEquals(0, response.getStatus().getSeverity());
-        assertEquals("ok", response.getStatus().getMessage());
-        waitForServerState("started", 10);
+        assertEquals(STATUS_MESSAGE_OK, response.getStatus().getMessage());
+        waitForServerState(STATE_STARTED, 10);
         
         serverProxy.stopServerAsync(new StopServerAttributes("wildfly3", true)).get();
-        waitForServerState("stopped", 10);
+        waitForServerState(STATE_STOPPED, 10);
     }
-    
+
     @Test
     public void testStartServerTwice() throws Exception {
         createServer(WILDFLY13_ROOT, "wildfly4");
+
         Map<String, Object> attr = new HashMap<>();
         attr.put("server.home.dir", WILDFLY13_ROOT);
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes(wfly13Type.getId(), "wildfly4", attr), "run");
+                new ServerAttributes(wfly13Type.getId(), "wildfly4", attr), MODE_RUN);
         
         serverProxy.startServerAsync(params).get();
-        waitForServerState("started", 10);
+        waitForServerState(STATE_STARTED, 10);
         StartServerResponse response = serverProxy.startServerAsync(params).get();
         
         assertEquals(Status.CANCEL, response.getStatus().getSeverity());
         
         serverProxy.stopServerAsync(new StopServerAttributes("wildfly4", true)).get();
-        waitForServerState("stopped", 10);
+        waitForServerState(STATE_STOPPED, 10);
     }
+
     @Test
     public void testStopServer() throws Exception {
         createServer(WILDFLY13_ROOT, "wildfly5");
         Map<String, Object> attr = new HashMap<>();
         attr.put("server.home.dir", WILDFLY13_ROOT);
         LaunchParameters params = new LaunchParameters(
-                new ServerAttributes(wfly13Type.getId(), "wildfly5", attr), "run");
+                new ServerAttributes(wfly13Type.getId(), "wildfly5", attr), MODE_RUN);
         
         serverProxy.startServerAsync(params).get();
-        waitForServerState("started", 10);
+        waitForServerState(STATE_STARTED, 10);
         
         Status status = serverProxy.stopServerAsync(new StopServerAttributes("wildfly5", false)).get();
-        waitForServerState("stopped", 10);
+        waitForServerState(STATE_STOPPED, 10);
         
         assertEquals(Status.OK, status.getSeverity());
-        assertEquals("ok", status.getMessage());
+        assertEquals(STATUS_MESSAGE_OK, status.getMessage());
     }
-    
+
     @Test
     public void testStopStoppedServer() throws Exception {
         createServer(WILDFLY13_ROOT, "wildfly6");
         Status status = serverProxy.stopServerAsync(new StopServerAttributes("wildfly6", false)).get();
         
-        assertEquals("The server is already marked as stopped. If you wish to force a stop request, please set the force flag to true.", status.getMessage());
+        assertEquals("The server is already marked as stopped. "
+        		+ "If you wish to force a stop request, please set the force flag to true.",
+        		status.getMessage());
         assertEquals(Status.ERROR, status.getSeverity());
     }
-    
+
     @Test
     public void testStopNonexistingServer() throws Exception {
         Status status = serverProxy.stopServerAsync(new StopServerAttributes("wildfly7", false)).get();
@@ -298,15 +308,15 @@ public class WildFlyLaunchingTest extends RSPCase {
         assertEquals(Status.ERROR, status.getSeverity());
     }
     
-    private void waitForServerState(String serverState, int timeout) throws Exception {
-        int tries = timeout;
-        
-        while(tries > 0) {
-            tries--;
+    private void waitForServerState(String serverState, int tries) throws Exception {
+        int remaining = tries;
+
+		while (remaining > 0) {
+			remaining--;
             if (serverState.equals(client.getStateString())) {
                 return;
             }
-            Thread.sleep(1000);
+            Thread.sleep(WAIT_FOR_SERVERSTATE);
         }
         throw new AssertionError("Waiting for server state to change to " + serverState + " timed out");
     }
