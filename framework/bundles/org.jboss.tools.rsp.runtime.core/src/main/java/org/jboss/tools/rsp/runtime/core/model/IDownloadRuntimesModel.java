d947f6d678d35a469ae2300f646ef96cc54cfd59
Rob Stryker <rob@oxbeef.net>
2019-02-05 12:24:55 -0500

    [197] Update javadoc for download runtimes
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- framework/bundles/org.jboss.tools.rsp.runtime.core/src/main/java/org/jboss/tools/rsp/runtime/core/model/IDownloadRuntimesModel.java
+++ framework/bundles/org.jboss.tools.rsp.runtime.core/src/main/java/org/jboss/tools/rsp/runtime/core/model/IDownloadRuntimesModel.java
@@ -16,13 +16,68 @@ import org.jboss.tools.rsp.eclipse.core.runtime.IProgressMonitor;
 
 public interface IDownloadRuntimesModel {
 
+	/**
+	 * Get an array of the currently-registered download runtime providers
+	 * 
+	 * @return 
+	 */
 	public IDownloadRuntimesProvider[] getDownloadRuntimeProviders();
-	public Map<String, DownloadRuntime> getOrLoadDownloadRuntimes(IProgressMonitor monitor);
-	public DownloadRuntime findDownloadRuntime(String id, IProgressMonitor monitor);
-	
+
+	/**
+	 * Add a download runtime provider to the model
+	 * 
+	 * @param provider
+	 */
 	public void addDownloadRuntimeProvider(IDownloadRuntimesProvider provider);
+	
+	/**
+	 * Remove a download runtime provider from the model
+	 * 
+	 * @param provider
+	 */
 	public void removeDownloadRuntimeProvider(IDownloadRuntimesProvider provider);
 
-	public IDownloadRuntimesProvider findProviderForRuntime(String id, IProgressMonitor monitor);
+
+	/**
+	 * Get a map of the current download runtimes id -> DownloadRuntime
+	 * 
+	 * If the current model's map is empty, execute the possibly long-running 
+	 * operation to ensure the model is full. 
+	 * 
+	 * @param monitor
+	 * @return
+	 */
+	public Map<String, DownloadRuntime> getOrLoadDownloadRuntimes(IProgressMonitor monitor);
+	
+	/**
+	 * Find a download runtime with the given id.
+	 *  
+	 * If the model is empty, this method will execute the possibly 
+	 * long-running operation to ensure that the model is full.
+	 *  
+	 * @param id
+	 * @param monitor
+	 * @return
+	 */
+	public DownloadRuntime findDownloadRuntime(String id, IProgressMonitor monitor);
+	
+	/**
+	 * Find the IDownloadRuntimesProvider for the given runtime id. 
+	 * If the model is empty or incomplete, return null. 
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public IDownloadRuntimesProvider findProviderForRuntime(String id);
+	
+	/**
+	 * Find the IDownloadRuntimesProvider for the given runtime id.
+	 *  
+	 * This method will execute the potentially long-running task of 
+	 * loading the model if it has not yet been loaded. 
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public IDownloadRuntimesProvider findProviderForRuntime(String id, IProgressMonitor monitor);
 }
