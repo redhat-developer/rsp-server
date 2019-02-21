b8c9b354f18a54c07e534d4b8ca74acd6afabf31
Rob Stryker <rob@oxbeef.net>
2019-02-21 13:03:05 -0500

    Upgrade to EPL 2.0
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- framework/bundles/org.jboss.tools.rsp.server/src/main/java/org/jboss/tools/rsp/server/core/internal/Base.java
+++ framework/bundles/org.jboss.tools.rsp.server/src/main/java/org/jboss/tools/rsp/server/core/internal/Base.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * Copyright (c) 2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
  * All rights reserved. This program is made available under the terms of the
- * Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
+ * Eclipse Public License v2.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v20.html
  * 
  * Contributors: Red Hat, Inc. IBM Corporation - Initial API and implementation
  ******************************************************************************/
