3c2196e46bc4325d1c4d94e460c05c9242a19d9b
Andre Dietisheim <adietish@redhat.com>
2019-02-27 14:38:58 +0100

    [249] added tests for DaoClasses
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

---
+++ api/docs/org.jboss.tools.rsp.schema/src/test/java/daopackage/subpackage1/DiscoveryPath.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
+package daopackage.subpackage1;
+
+public class DiscoveryPath {
+	private String filepath;
+
+	public DiscoveryPath() {
+
+	}
+
+	public DiscoveryPath(String filepath) {
+		super();
+		this.setFilepath(filepath);
+	}
+
+	public String getFilepath() {
+		return filepath;
+	}
+
+	public void setFilepath(String filepath) {
+		this.filepath = filepath;
+	}
+
+	@Override
+	public int hashCode() {
+		return filepath.hashCode();
+	}
+
+	public boolean equals(Object other) {
+		if (other instanceof DiscoveryPath) {
+			String fp = ((DiscoveryPath) other).getFilepath();
+			if (this.getFilepath() == null)
+				return fp == null;
+			return this.getFilepath().equals(fp);
+		}
+		return false;
+	}
+
+}
