f59045f05f502c70d5c2dbbc4e7173f71f08435e
Rob Stryker <rob@oxbeef.net>
2019-05-06 14:59:57 -0400

    vscode-adapters-309 - Natural ordering for getServerTypes or download runtime
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ framework/bundles/org.jboss.tools.rsp.server.spi/src/main/java/org/jboss/tools/rsp/server/spi/util/AlphanumComparator.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc. Distributed under license by Red Hat, Inc.
+ * All rights reserved. This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors: Red Hat, Inc.
+ ******************************************************************************/
+package org.jboss.tools.rsp.server.spi.util;
+
+import java.util.Comparator;
+
+public class AlphanumComparator implements Comparator<String> {
+	public AlphanumComparator() {
+	}
+
+	private final boolean isDigit(char ch) {
+		return ((ch >= 48) && (ch <= 57));
+	}
+
+	/**
+	 * Length of string is passed in for improved efficiency (only need to calculate
+	 * it once)
+	 **/
+	private final String getChunk(String s, int slength, int marker) {
+		StringBuilder chunk = new StringBuilder();
+		char c = s.charAt(marker);
+		chunk.append(c);
+		marker++;
+		if (isDigit(c)) {
+			while (marker < slength) {
+				c = s.charAt(marker);
+				if (!isDigit(c))
+					break;
+				chunk.append(c);
+				marker++;
+			}
+		} else {
+			while (marker < slength) {
+				c = s.charAt(marker);
+				if (isDigit(c))
+					break;
+				chunk.append(c);
+				marker++;
+			}
+		}
+		return chunk.toString();
+	}
+
+	public int compare(String s1, String s2) {
+		if ((s1 == null) || (s2 == null)) {
+			return 0;
+		}
+
+		int thisMarker = 0;
+		int thatMarker = 0;
+		int s1Length = s1.length();
+		int s2Length = s2.length();
+
+		while (thisMarker < s1Length && thatMarker < s2Length) {
+			String thisChunk = getChunk(s1, s1Length, thisMarker);
+			thisMarker += thisChunk.length();
+
+			String thatChunk = getChunk(s2, s2Length, thatMarker);
+			thatMarker += thatChunk.length();
+
+			// If both chunks contain numeric characters, sort them numerically
+			int result = 0;
+			if (isDigit(thisChunk.charAt(0)) && isDigit(thatChunk.charAt(0))) {
+				// Simple chunk comparison by length.
+				int thisChunkLength = thisChunk.length();
+				result = thisChunkLength - thatChunk.length();
+				// If equal, the first different number counts
+				if (result == 0) {
+					for (int i = 0; i < thisChunkLength; i++) {
+						result = thisChunk.charAt(i) - thatChunk.charAt(i);
+						if (result != 0) {
+							return result;
+						}
+					}
+				}
+			} else {
+				result = thisChunk.compareTo(thatChunk);
+			}
+
+			if (result != 0)
+				return result;
+		}
+
+		return s1Length - s2Length;
+	}
+}
