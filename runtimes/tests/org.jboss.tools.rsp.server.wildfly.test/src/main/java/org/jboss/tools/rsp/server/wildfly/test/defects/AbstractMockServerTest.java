b8f2c9968b300f6ad3af65b122835c70b604544c
Rob Stryker <rob@oxbeef.net>
2019-05-14 17:24:26 -0400

    Add a test for issue353
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ runtimes/tests/org.jboss.tools.rsp.server.wildfly.test/src/main/java/org/jboss/tools/rsp/server/wildfly/test/defects/AbstractMockServerTest.java
@@ -0,0 +1,50 @@
+package org.jboss.tools.rsp.server.wildfly.test.defects;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import org.jboss.tools.rsp.server.spi.discovery.IServerBeanTypeManager;
+import org.jboss.tools.rsp.server.spi.discovery.ServerBeanType;
+import org.jboss.tools.rsp.server.spi.model.IServerManagementModel;
+import org.jboss.tools.rsp.server.spi.model.IServerModel;
+import org.jboss.tools.rsp.server.spi.servertype.IServer;
+import org.jboss.tools.rsp.server.spi.servertype.IServerType;
+import org.jboss.tools.rsp.server.wildfly.servertype.IJBossServerAttributes;
+
+public abstract class AbstractMockServerTest {
+
+	
+	protected IServer createServer(String type) {
+		IServer s = mock(IServer.class);
+		IServerType st = mock(IServerType.class);
+		doReturn(st).when(s).getServerType();
+		doReturn(type).when(st).getId();
+		doReturn(".").when(s).getAttribute(IJBossServerAttributes.SERVER_HOME, (String)null);
+
+		IServerManagementModel managementModel = mockServerManagementModel();
+		doReturn(managementModel).when(s).getServerManagementModel();
+	
+		IServerModel sm = mockServerModel();
+		doReturn(sm).when(managementModel).getServerModel();
+		return s;
+	}
+
+	protected IServerManagementModel mockServerManagementModel() {
+		IServerManagementModel managementModel = mock(IServerManagementModel.class);		
+		IServerBeanTypeManager beanTypeManager = mockServerBeanTypeManager();
+		doReturn(beanTypeManager).when(managementModel).getServerBeanTypeManager();
+		return managementModel;
+	}
+
+	protected IServerBeanTypeManager mockServerBeanTypeManager() {
+		IServerBeanTypeManager beanTypeManager = mock(IServerBeanTypeManager.class);
+		doReturn(new ServerBeanType[] {}).when(beanTypeManager).getAllRegisteredTypes();
+		return beanTypeManager;
+	}
+
+	protected IServerModel mockServerModel() {
+		IServerModel model = mock(IServerModel.class);
+		return model;
+	}
+
+}
