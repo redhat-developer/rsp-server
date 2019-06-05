e850c3cd9a887906239eb496164e790725408390
Rob Stryker <rob@oxbeef.net>
2019-06-05 15:46:50 -0400

    Failing Test
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- runtimes/tests/org.jboss.tools.rsp.server.wildfly.test/src/main/java/org/jboss/tools/rsp/server/wildfly/test/beans/JBossServerBeanLoaderTest.java
+++ runtimes/tests/org.jboss.tools.rsp.server.wildfly.test/src/main/java/org/jboss/tools/rsp/server/wildfly/test/beans/JBossServerBeanLoaderTest.java
@@ -88,6 +88,7 @@ public class JBossServerBeanLoaderTest extends TestCase {
 		expected.put(IServerConstants.SERVER_WILDFLY_140, new Data(JBossServerBeanTypeProvider.WILDFLY140, "14.0."));
 		expected.put(IServerConstants.SERVER_WILDFLY_150, new Data(JBossServerBeanTypeProvider.WILDFLY150, "15.0."));
 		expected.put(IServerConstants.SERVER_WILDFLY_160, new Data(JBossServerBeanTypeProvider.WILDFLY160, "16.0."));
+		expected.put(IServerConstants.SERVER_WILDFLY_170, new Data(JBossServerBeanTypeProvider.WILDFLY170, "17.0."));
 		expected.put(IServerConstants.SERVER_EAP_43, new Data(JBossServerBeanTypeProvider.EAP_STD, "4.3."));
 		expected.put(IServerConstants.SERVER_EAP_50, new Data(JBossServerBeanTypeProvider.EAP_STD, "5.1."));
 		expected.put(IServerConstants.SERVER_EAP_60, new Data(JBossServerBeanTypeProvider.EAP6, "6.0."));
