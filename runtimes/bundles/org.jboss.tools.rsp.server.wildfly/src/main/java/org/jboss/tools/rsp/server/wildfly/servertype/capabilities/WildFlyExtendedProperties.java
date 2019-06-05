a1a65e3af6fc6cea5ab92539546b3f1b85cbcef7
Rob Stryker <rob@oxbeef.net>
2019-06-05 14:33:17 -0400

    WF17 initial impl
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ runtimes/bundles/org.jboss.tools.rsp.server.wildfly/src/main/java/org/jboss/tools/rsp/server/wildfly/servertype/capabilities/WildFlyExtendedProperties.java
@@ -0,0 +1,70 @@
+/******************************************************************************* 
+ * Copyright (c) 2019 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v2.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v20.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.rsp.server.wildfly.servertype.capabilities;
+
+import org.jboss.tools.rsp.server.spi.servertype.IServer;
+import org.jboss.tools.rsp.server.wildfly.servertype.launch.Wildfly100DefaultLaunchArguments;
+import org.jboss.tools.rsp.server.wildfly.servertype.launch.Wildfly110DefaultLaunchArguments;
+import org.jboss.tools.rsp.server.wildfly.servertype.launch.Wildfly80DefaultLaunchArguments;
+
+public class WildFlyExtendedProperties {
+	public static class Wildfly80ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly80ExtendedProperties(IServer server) {
+			super("8.x", "1.7", "1.8", "service:jmx:http-remoting-jmx", new Wildfly80DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly90ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly90ExtendedProperties(IServer server) {
+			super("9.x", "1.7", "1.8", "service:jmx:remote+http", new Wildfly80DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly100ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly100ExtendedProperties(IServer server) {
+			super("10.x", "1.8", "1.8", "service:jmx:remote+http", new Wildfly100DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly110ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly110ExtendedProperties(IServer server) {
+			super("11.0", "1.8", "9.", "service:jmx:remote+http", new Wildfly100DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly120ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly120ExtendedProperties(IServer server) {
+			super("12.0", "1.8", "10.", "service:jmx:remote+http", new Wildfly100DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly130ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly130ExtendedProperties(IServer server) {
+			super("13.0", "1.8", "11.0", "service:jmx:remote+http", new Wildfly110DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly140ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly140ExtendedProperties(IServer server) {
+			super("14", "1.8", "11.0", "service:jmx:remote+http", new Wildfly110DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly150ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly150ExtendedProperties(IServer server) {
+			super("15.0", "1.8", "11.", "service:jmx:remote+http", new Wildfly110DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly160ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly160ExtendedProperties(IServer server) {
+			super("16.0", "1.8", "12.", "service:jmx:remote+http", new Wildfly110DefaultLaunchArguments(server), server);
+		}
+	}
+	public static class Wildfly170ExtendedProperties extends AbstractWildflyExtendedProperties {
+		public Wildfly170ExtendedProperties(IServer server) {
+			super("17.0", "1.8", "12.", "service:jmx:remote+http", new Wildfly110DefaultLaunchArguments(server), server);
+		}
+	}
+
+}
