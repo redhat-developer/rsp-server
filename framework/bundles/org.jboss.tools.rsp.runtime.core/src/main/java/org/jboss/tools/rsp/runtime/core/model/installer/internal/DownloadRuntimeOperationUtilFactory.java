d1522e167012a83857021012899f12e146f5daf1
Rob Stryker <rob@oxbeef.net>
2020-01-08 12:50:22 -0500

    Add copyright header
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- framework/bundles/org.jboss.tools.rsp.runtime.core/src/main/java/org/jboss/tools/rsp/runtime/core/model/installer/internal/DownloadRuntimeOperationUtilFactory.java
+++ framework/bundles/org.jboss.tools.rsp.runtime.core/src/main/java/org/jboss/tools/rsp/runtime/core/model/installer/internal/DownloadRuntimeOperationUtilFactory.java
@@ -1,3 +1,13 @@
+/*************************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
 package org.jboss.tools.rsp.runtime.core.model.installer.internal;
 
 import java.io.InputStream;
