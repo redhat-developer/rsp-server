20d4e72a40f0940a295ff21e107941c24e9d761c
Rob Stryker <rob@oxbeef.net>
2020-09-04 15:06:05 -0400

    Fixes vscode-server-connector 465 - multistatus messages not transmitted properly
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- framework/bundles/org.jboss.tools.rsp.server.spi/src/main/java/org/jboss/tools/rsp/server/spi/util/StatusConverter.java
+++ framework/bundles/org.jboss.tools.rsp.server.spi/src/main/java/org/jboss/tools/rsp/server/spi/util/StatusConverter.java
@@ -11,6 +11,9 @@ package org.jboss.tools.rsp.server.spi.util;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.jboss.tools.rsp.eclipse.core.runtime.IStatus;
+import org.jboss.tools.rsp.eclipse.core.runtime.MultiStatus;
+
 public class StatusConverter {
 	public static org.jboss.tools.rsp.eclipse.core.runtime.IStatus convert(
 			org.jboss.tools.rsp.api.dao.Status status) {
@@ -25,10 +28,36 @@ public class StatusConverter {
 		if (status == null) {
 			return null;
 		}
+		StringBuffer fullMessage = new StringBuffer();
+
 		int sev = status.getSeverity();
 		String plugin = status.getPlugin();
 		String msg = status.getMessage();
 		Throwable t = status.getException();
+		String trace = getTrace(t);
+		fullMessage.append(trace);
+		
+		if( status instanceof MultiStatus ) {
+			IStatus[] kids = ((MultiStatus)status).getChildren();
+			if( kids != null ) {
+				for( int i = 0; i < kids.length; i++ ) {
+					String msg2 = status.getMessage();
+					Throwable t2 = status.getException();
+					String trace2 = getTrace(t2);
+					fullMessage.append("\n\n");
+					fullMessage.append(msg2);
+					fullMessage.append("\n");
+					fullMessage.append(trace2);
+				}
+			}
+		}
+		
+		org.jboss.tools.rsp.api.dao.Status ret = 
+				new org.jboss.tools.rsp.api.dao.Status(sev, plugin, msg, fullMessage.toString());
+		return ret;
+	}
+	
+	private static String getTrace(Throwable t) {
 		String trace = null;
 		if( t != null ) {
 			StringWriter sw = new StringWriter();
@@ -36,8 +65,7 @@ public class StatusConverter {
 			t.printStackTrace(pw);
 			trace = sw.toString(); // stack trace as a string
 		}
-		org.jboss.tools.rsp.api.dao.Status ret = 
-				new org.jboss.tools.rsp.api.dao.Status(sev, plugin, msg, trace);
-		return ret;
+		return trace;
+
 	}
 }
